255b848b5cf81ce3583617fc6d6474ef760ffa75
Antoine BERNIER <antoine.bernier@gmail.com>
2019-11-01 13:03:19 +0100

    new build

--- packages/acme/compA/dist/index.esm.js
+++ packages/acme/compA/dist/index.esm.js
@@ -1,103 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
 
-function _classCallCheck(instance, Constructor) {
-  if (!(instance instanceof Constructor)) {
-    throw new TypeError("Cannot call a class as a function");
-  }
-}
-
-function _defineProperties(target, props) {
-  for (var i = 0; i < props.length; i++) {
-    var descriptor = props[i];
-    descriptor.enumerable = descriptor.enumerable || false;
-    descriptor.configurable = true;
-    if ("value" in descriptor) descriptor.writable = true;
-    Object.defineProperty(target, descriptor.key, descriptor);
-  }
-}
-
-function _createClass(Constructor, protoProps, staticProps) {
-  if (protoProps) _defineProperties(Constructor.prototype, protoProps);
-  if (staticProps) _defineProperties(Constructor, staticProps);
-  return Constructor;
-}
-
-function _inherits(subClass, superClass) {
-  if (typeof superClass !== "function" && superClass !== null) {
-    throw new TypeError("Super expression must either be null or a function");
+function _taggedTemplateLiteral(strings, raw) {
+  if (!raw) {
+    raw = strings.slice(0);
   }
 
-  subClass.prototype = Object.create(superClass && superClass.prototype, {
-    constructor: {
-      value: subClass,
-      writable: true,
-      configurable: true
+  return Object.freeze(Object.defineProperties(strings, {
+    raw: {
+      value: Object.freeze(raw)
     }
-  });
-  if (superClass) _setPrototypeOf(subClass, superClass);
+  }));
 }
 
-function _getPrototypeOf(o) {
-  _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) {
-    return o.__proto__ || Object.getPrototypeOf(o);
-  };
-  return _getPrototypeOf(o);
-}
+function _templateObject() {
+  var data = _taggedTemplateLiteral(["\n  background: papayawhip;\n  color: ", ";\n\n  &:hover {\n    background: ", ";\n    color: papayawhip;\n  }\n"]);
 
-function _setPrototypeOf(o, p) {
-  _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) {
-    o.__proto__ = p;
-    return o;
+  _templateObject = function _templateObject() {
+    return data;
   };
 
-  return _setPrototypeOf(o, p);
+  return data;
 }
 
-function _assertThisInitialized(self) {
-  if (self === void 0) {
-    throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
-  }
+var StyledTest = styled.div(_templateObject(), function (props) {
+  return props.color || "palevioletred";
+}, function (props) {
+  return props.color || "palevioletred";
+});
 
-  return self;
+function Test(props) {
+  return React.createElement(StyledTest, props, props.children);
 }
 
-function _possibleConstructorReturn(self, call) {
-  if (call && (typeof call === "object" || typeof call === "function")) {
-    return call;
-  }
-
-  return _assertThisInitialized(self);
-}
-
-var HelloWorldA =
-/*#__PURE__*/
-function (_React$Component) {
-  _inherits(HelloWorldA, _React$Component);
-
-  function HelloWorldA(props) {
-    var _this;
-
-    _classCallCheck(this, HelloWorldA);
-
-    _this = _possibleConstructorReturn(this, _getPrototypeOf(HelloWorldA).call(this, props));
-    _this.state = {
-      message: "Hello WorldA!"
-    };
-    return _this;
-  }
-
-  _createClass(HelloWorldA, [{
-    key: "render",
-    value: function render() {
-      var _this$state;
-
-      return React.createElement("div", null, React.createElement("h1", null, (_this$state = this.state) === null || _this$state === void 0 ? void 0 : _this$state.message), React.createElement("style", {
-        jsx: true
-      }, "\n          h1 {\n            color: red;\n            background: gray;\n          }\n        "));
-    }
-  }]);
-
-  return HelloWorldA;
-}(React.Component);
-
-export default HelloWorldA;
+export default Test;
