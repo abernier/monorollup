9b3cb5fb28a56518d220fa706f683dab4b03cfae
abernier <antoine.bernier@gmail.com>
2020-10-02 15:49:53 +0200

    tweak

--- packages/acme/compA/index.js
+++ packages/acme/compA/index.js
@@ -10,7 +10,7 @@ const StyledTest = styled.div`
     background: ${props => props.color || "palevioletred"};
     color: papayawhip;
   }
-`;
+`
 
 function Test(props) {
   return <StyledTest {...props}>{props.children}</StyledTest>
