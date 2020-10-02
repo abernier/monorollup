a28e641c3e4201dfe66ae3448afb4efa28a7fd36
abernier <antoine.bernier@gmail.com>
2020-10-02 15:47:54 +0200

    adddistfiles script

--- lerna-exec/foo.js
+++
@@ -1,7 +0,0 @@
-const lernaRootPath = process.env.LERNA_ROOT_PATH // absolute root path of the monorepo
-const packageRootPath = process.cwd() // absolute path of the current package
-
-const pkgPath = `${packageRootPath}/package.json`
-const pkg = require(pkgPath)
-
-console.log([pkg.main, pkg.module].filter(el => el).join(' '))
